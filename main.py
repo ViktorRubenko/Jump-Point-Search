73c8661b6ec47df8608ed883c33b64a11ae6c3e2
ViktorRubenko <vrubenko@gmail.com>
2019-10-23 11:36:10 +0300

    Update main.py
    English desc + code formatting

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
-import pygame, grid, astar, jps, os, datetime
+import pygame
+import os
+import datetime
+import grid
+import astar
+import jps
+
 
 def main():
 
-    print('Эвристика Манхэттена:\n1 - вызов алгоритма A*'+
-          '\n2 - вызов алгоритма JPS\n3 - вызов обоих методов\n'+
-          '\nЭвристика Евклида:\n5 - вызов алгоритма A*'+
-          '\n6 - вызов алгоритма JPS\n7 - вызов обоих методов\n'+
-          '\nF5 - очистить сетку\n')
+    print(
+        "Right click to set start node and end node"
+        + "\nHold left button to draw obstacles"
+        + "\nButtons for calling algorithms:"
+        + "\nManhattan heuristic:\n1 - call A* algorithm"
+        + "\n2 - call JPS algorithm\n3 - call both\n"
+        + "\nEuclidean heuristic:\n5 - call A* algorithm"
+        + "\n6 - call JPS algorithm\n7 - call both\n"
+        + "\nF5 - clear grid\n"
+    )
 
     while True:
         try:
-            size1 = int(input('Размер окна:\nШирина: '))
-            size2 = int(input('Высота: '))
-            size_node = int(input('Размер узла: '))
-            if size1 % size_node !=0 or size2 % size_node !=0:
-                print('Высота и ширина должны быть кратны размеру узла\n')
-            else: break
+            width, height, size_node = map(
+                int,
+                [
+                    input(x)
+                    for x in ("Window width: ", "Window height: ", "Node size: ")
+                ],
+            )
+            if width % size_node != 0 or height % size_node != 0:
+                print(
+                    "Width and height should be fully divided by the size of the node\n"
+                )
+            else:
+                break
         except:
-            size1,size2 = 800,640
+            width, height = 800, 640
             size_node = 16
             break
-            
-    size = (size1,size2)
+
+    size = (width, height)
 
     pygame.init()
     screen = pygame.display.set_mode(size)
     running = True
     mark_border = False
     clear_border = False
-    
+
     background = pygame.Surface(size)
     Grid = grid.Grid(size, size_node)
     Grid.refresh(background)
@@ -40,14 +58,16 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     pos = pygame.mouse.get_pos()
-                    if Grid.matrix[pos[1]//size_node][pos[0]//size_node] == 0:
+                    if (
+                        Grid.matrix[pos[1] // size_node][pos[0] // size_node]
+                        == 0
+                    ):
                         mark_border = True
                     else:
                         clear_border = True
                 elif event.button == 3:
-                    Grid.mark_node(pygame.mouse.get_pos(),background)
-            if (event.type == pygame.MOUSEBUTTONUP and
-                event.button == 1):
+                    Grid.mark_node(pygame.mouse.get_pos(), background)
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 mark_border = False
                 clear_border = False
             if event.type == pygame.KEYDOWN:
@@ -55,40 +75,66 @@ def main():
                     Grid.refresh(background)
                 if event.key == pygame.K_1:
                     Grid.lightrefresh(background)
-                    Grid.drawpath(astar.method(Grid.matrix,Grid.start,Grid.goal,1),
-                                  background,1)
+                    Grid.drawpath(
+                        astar.method(Grid.matrix, Grid.start, Grid.goal, 1),
+                        background,
+                        1,
+                    )
                 if event.key == pygame.K_2:
                     Grid.lightrefresh(background)
-                    Grid.drawpath(jps.method(Grid.matrix,Grid.start,Grid.goal,1),
-                                  background,2)
+                    Grid.drawpath(
+                        jps.method(Grid.matrix, Grid.start, Grid.goal, 1),
+                        background,
+                        2,
+                    )
                 if event.key == pygame.K_3:
                     Grid.lightrefresh(background)
-                    Grid.drawpath(astar.method(Grid.matrix,Grid.start,Grid.goal,1),
-                                  background,1)
-                    Grid.drawpath(jps.method(Grid.matrix,Grid.start,Grid.goal,1),
-                                  background,2)
+                    Grid.drawpath(
+                        astar.method(Grid.matrix, Grid.start, Grid.goal, 1),
+                        background,
+                        1,
+                    )
+                    Grid.drawpath(
+                        jps.method(Grid.matrix, Grid.start, Grid.goal, 1),
+                        background,
+                        2,
+                    )
                 if event.key == pygame.K_5:
                     Grid.lightrefresh(background)
-                    Grid.drawpath(astar.method(Grid.matrix,Grid.start,Grid.goal,2),
-                                  background,1)
+                    Grid.drawpath(
+                        astar.method(Grid.matrix, Grid.start, Grid.goal, 2),
+                        background,
+                        1,
+                    )
                 if event.key == pygame.K_6:
                     Grid.lightrefresh(background)
-                    Grid.drawpath(jps.method(Grid.matrix,Grid.start,Grid.goal,2),
-                                  background,2)
+                    Grid.drawpath(
+                        jps.method(Grid.matrix, Grid.start, Grid.goal, 2),
+                        background,
+                        2,
+                    )
                 if event.key == pygame.K_7:
                     Grid.lightrefresh(background)
-                    Grid.drawpath(astar.method(Grid.matrix,Grid.start,Grid.goal,2),
-                                  background,1)
-                    Grid.drawpath(jps.method(Grid.matrix,Grid.start,Grid.goal,2),
-                                  background,2)
-        screen.blit(background,(0,0))
+                    Grid.drawpath(
+                        astar.method(Grid.matrix, Grid.start, Grid.goal, 2),
+                        background,
+                        1,
+                    )
+                    Grid.drawpath(
+                        jps.method(Grid.matrix, Grid.start, Grid.goal, 2),
+                        background,
+                        2,
+                    )
+        screen.blit(background, (0, 0))
         pygame.display.update()
 
         if mark_border:
-            Grid.mark_border(pygame.mouse.get_pos(),background)
+            Grid.mark_border(pygame.mouse.get_pos(), background)
         if clear_border:
-            Grid.clear_border(pygame.mouse.get_pos(),background)
-                
+            Grid.clear_border(pygame.mouse.get_pos(), background)
+
     pygame.quit()
 
-if __name__=='__main__': main()
+
+if __name__ == "__main__":
+    main()
